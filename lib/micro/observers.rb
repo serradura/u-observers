c8f790a2135a93f0e7901a64fe8a1b36e59fac8e
Rodrigo Serradura <rodrigo.serradura@gmail.com>
2020-09-25 09:33:11 -0300

    First commit

---
+++ lib/micro/observers.rb
@@ -0,0 +1,8 @@
+require "micro/observers/version"
+
+module Micro
+  module Observers
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/micro/observers/version.rb
@@ -0,0 +1,5 @@
+module Micro
+  module Observers
+    VERSION = "0.1.0"
+  end
+end
