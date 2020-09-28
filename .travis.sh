a5b2f7b0899f4038cc6e92f6d9056a4919cfae13
Rodrigo Serradura <rodrigo.serradura@gmail.com>
2020-09-28 15:14:15 -0300

    Run CI with different Ruby/ActiveRecord versions

---
+++ .travis.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+ruby_v=$(ruby -v)
+
+ACTIVERECORD_VERSION='3.2' bundle update
+ACTIVERECORD_VERSION='3.2' bundle exec rake test
+
+ACTIVERECORD_VERSION='4.0' bundle update
+ACTIVERECORD_VERSION='4.0' bundle exec rake test
+
+ACTIVERECORD_VERSION='4.1' bundle update
+ACTIVERECORD_VERSION='4.1' bundle exec rake test
+
+ACTIVERECORD_VERSION='4.2' bundle update
+ACTIVERECORD_VERSION='4.2' bundle exec rake test
+
+ACTIVERECORD_VERSION='5.0' bundle update
+ACTIVERECORD_VERSION='5.0' bundle exec rake test
+
+ACTIVERECORD_VERSION='5.1' bundle update
+ACTIVERECORD_VERSION='5.1' bundle exec rake test
+
+if [[ ! $ruby_v =~ '2.2.0' ]]; then
+  ACTIVERECORD_VERSION='5.2' bundle update
+  ACTIVERECORD_VERSION='5.2' bundle exec rake test
+fi
+
+if [[ $ruby_v =~ '2.5.' ]] || [[ $ruby_v =~ '2.6.' ]] || [[ $ruby_v =~ '2.7.' ]]; then
+  ACTIVERECORD_VERSION='6.0' bundle update
+  ACTIVERECORD_VERSION='6.0' bundle exec rake test
+fi
