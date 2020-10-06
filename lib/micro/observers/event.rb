f343f456b20582994f363635523da4719cec9357
Rodrigo Serradura <rodrigo.serradura@taqtile.com>
2020-10-05 22:59:23 -0300

    Add Micro::Observers::Event to represents the notification payload

--- lib/micro/observers/event.rb
+++ lib/micro/observers/event.rb
@@ -3,8 +3,18 @@
 module Micro
   module Observers
 
-    module Event
+    class Event
       require 'micro/observers/event/names'
+
+      attr_reader :name, :subject, :context, :data
+
+      def initialize(name, subject, context, data)
+        @name, @subject = name, subject
+        @context, @data = context, data
+      end
+
+      alias ctx context
+      alias subj subject
     end
 
   end
